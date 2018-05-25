5997a4e3b7867870db896f32b548ea9cd4258e7a
Alistair <1011776@student.ccgs.wa.edu.au>
2018-05-25 18:07:15 +0800

    Create prototypes for C4GameAI

---
+++ C4GameAI.c
@@ -0,0 +1,7 @@
+// playC4Game.c
+// Created by Alistair Parkinson
+// 25/5/2018
+
+void makeMove (C4Game game) {
+
+}
---
+++ C4GameAI.h
@@ -0,0 +1,7 @@
+// C4GameAI.h
+// Created by Alistair Parkinson
+// 28/4/2018
+
+#include "C4Game.h"
+
+void makeMove (C4Game game);
